44d48f6e035e54f1e4543a36a3c5b4aaab196f4d
Andrew Gallagher <andrew.gallagher@notiv.com>
2020-01-07 11:51:57 +1000

    add all the fields

--- lib/onesignal/player.rb
+++ lib/onesignal/player.rb
@@ -2,7 +2,10 @@
 
 module OneSignal
   class Player
-    attr_reader :device_type
+    attr_reader :device_type, :email_auth_hash, :identifier, :language, :timezone, :game_version,
+      :device_model, :device_os, :ad_id, :sdk, :session_count, :tags, :amount_spent, :created_at,
+      :playtime, :badge_count, :last_active, :notification_types, :test_type, :long, :lat, :country,
+      :external_user_id
 
     def initialize **params
       puts params.inspect
@@ -10,12 +13,56 @@ module OneSignal
         raise ArgumentError, 'missing device_type'
       end
 
-      @device_type = params[:device_type]
+      @device_type        = params[:device_type]
+      @email_auth_hash    = params[:email_auth_hash]
+      @identifier         = params[:identifier]
+      @language           = params[:language]
+      @timezone           = params[:timezone]
+      @game_version       = params[:game_version]
+      @device_model       = params[:device_model]
+      @device_os          = params[:device_os]
+      @ad_id              = params[:ad_id]
+      @sdk                = params[:sdk]
+      @session_count      = params[:session_count]
+      @tags               = params[:tags]
+      @amount_spent       = params[:amount_spent]
+      @created_at         = params[:created_at]
+      @playtime           = params[:playtime]
+      @badge_count        = params[:badge_count]
+      @last_active        = params[:last_active]
+      @notification_types = params[:notification_types]
+      @test_type          = params[:test_type]
+      @long               = params[:long]
+      @lat                = params[:lat]
+      @country            = params[:country]
+      @external_user_id   = params[:external_user_id]
     end
 
     def as_json options = nil
       {
-        'device_type' => @device_type
+        'device_type'        => @device_type,
+        'email_auth_hash'    => @email_auth_hash,
+        'identifier'         => @identifier,
+        'language'           => @language,
+        'timezone'           => @timezone,
+        'game_version'       => @game_version,
+        'device_model'       => @device_model,
+        'device_os'          => @device_os,
+        'ad_id'              => @ad_id,
+        'sdk'                => @sdk,
+        'session_count'      => @session_count,
+        'tags'               => @tags,
+        'amount_spent'       => @amount_spent,
+        'created_at'         => @created_at,
+        'playtime'           => @playtime,
+        'badge_count'        => @badge_count,
+        'last_active'        => @last_active,
+        'notification_types' => @notification_types,
+        'test_type'          => @test_type,
+        'long'               => @long,
+        'lat'                => @lat,
+        'country'            => @country,
+        'external_user_id'   => @external_user_id
       }
     end
   end
--- spec/factories/player.rb
+++ spec/factories/player.rb
@@ -3,6 +3,28 @@
 FactoryBot.define do
   factory :player, class: OneSignal::Player do
     device_type { 1 }
+    email_auth_hash {}
+    identifier { '7abcd558f29d0b1f048083e2834ad8ea4b3d87d8ad9c088b33c132706ff445f0' }
+    language { 'en' }
+    timezone { 0 }
+    game_version { '1.0' }
+    device_model { 'iPhone8,2'}
+    device_os { '13.2' }
+    ad_id { '' }
+    sdk { '' }
+    session_count { 1 }
+    tags { { 'foo' => 'bar' } } 
+    amount_spent { 99.99 }
+    created_at { 0 }
+    playtime { 86400 }
+    badge_count { 1 }
+    last_active { '2020-01-01 12:00:00.GB' } 
+    notification_types { 1 }
+    test_type { 1 }
+    long { 0.00 }
+    lat { 0.00 }
+    country { 'GB' }
+    external_user_id { 'test' }
 
     initialize_with { new(attributes) }
   end
